c17fe788ad7e1eaf7321aa014822a2fd2d8042ca
funnywolf <dllaolang@126.com>
2018-07-29 16:56:54 +0800

    complete ListTest

--- src/list/ListTest.java
+++ src/list/ListTest.java
@@ -10,7 +10,12 @@ public class ListTest {
         for(int i = 0; i < n; i++) {
             list.insert(i, i);
         }
+        for(int i = 0; i < n; i++) {
+            list.insert(i, i);
+        }
         System.out.println("List: " + list.toString());
+        list.removeDuplicate();
+        System.out.println("removeDuplicate: " + list.toString());
         list.remove(n - 1);
         System.out.println("remove List[" + (n - 1) + "]: " + list.toString());
         list.remove(n / 2);
@@ -21,6 +26,26 @@ public class ListTest {
         System.out.println("reverse List: " + list.toString());
         System.out.println("get " + (n / 2) + " from tail: " + list.getFromTail(n / 2));
         System.out.println("getMiddle: " + list.getMiddle());
+        System.out.println("toStringFromTail: " + list.toStringFromTail());
+        list.tailPointTo(list.getLength() - 1);
+        System.out.println("tailPointTo(" + (list.getLength() - 1) + "), isLoop: " + list.isLoop());
+        SimpleList<Integer> list1 = new SimpleList<>();
+        for(int i = 0; i < n; i++) {
+            list1.insert(i, i);
+        }
+        SimpleList<Integer> list2 = new SimpleList<>();
+        for(int i = 0; i < n; i++) {
+            list2.insert(i, i);
+        }
+        System.out.println("list1(" + list1.getLength() + "): " + list1
+                + ", list2(" + list2.getLength() + "): " + list2);
+        list1.tailPointTo(list2, list2.getLength() / 2);
+        System.out.println("list1.tailPointTo(list2, " + (list2.getLength() / 2) + ")");
+        System.out.println("isIntersect: " + list1.isIntersect(list2));
+        System.out.println("list1(" + list1.getLength() + "): " + list1
+                + ", list2(" + list2.getLength() + "): " + list2);
+        System.out.println("list1.intersectAt(list2): " + list1.intersectAt(list2));
+        System.out.println("list2.intersectAt(list1): " + list2.intersectAt(list1));
         System.out.println("simpleListTest start");
     }
 }
--- src/list/SimpleList.java
+++ src/list/SimpleList.java
@@ -1,5 +1,9 @@
 package list;
 
+import stack.Stack;
+
+import java.util.HashSet;
+
 public class SimpleList<T> {
     private Node<T> mHead = new Node<>(null);
     //private Node<T> mTail = null;
@@ -10,27 +14,18 @@ public class SimpleList<T> {
     }
 
     public T get(int position) {
-        if(position < 0 || position >= length || mHead.next == null) {
-            return null;
-        }
-        Node<T> p = mHead.next;
-        while (position-- > 0) {
-            p = p.next;
-        }
-        return p.data;
+        Node<T> node = getNode(position);
+        return node == null ? null : node.data;
     }
 
     public void insert(int position, T data) {
         if(position < 0 || position > length) {
             return;
         }
-        Node<T> p = mHead;
-        while (position-- > 0) {
-            p = p.next;
-        }
+        Node<T> prev = position == 0 ? mHead : getNode(position - 1);
         Node<T> newNode = new Node<>(data);
-        newNode.next = p.next;
-        p.next = newNode;
+        newNode.next = prev.next;
+        prev.next = newNode;
         length++;
     }
 
@@ -38,14 +33,7 @@ public class SimpleList<T> {
         if(position < 0 || position >= length) {
             return;
         }
-        Node<T> p = mHead;
-        while (position-- > 0) {
-            p = p.next;
-        }
-        Node<T> tmp = p.next;
-        p.next = p.next.next;
-        tmp.next = null;
-        length--;
+        removeNode(position == 0 ? mHead : getNode(position - 1));
     }
 
     public void reverse() {
@@ -65,10 +53,7 @@ public class SimpleList<T> {
         if(position < 0 || position >= length) {
             return null;
         }
-        Node<T> fast = mHead.next;
-        while (position-- > 0) {
-            fast = fast.next;
-        }
+        Node<T> fast = getNode(position);
         Node<T> slow = mHead.next;
         while (fast.next != null) {
             fast = fast.next;
@@ -90,6 +75,135 @@ public class SimpleList<T> {
         return slow.data;
     }
 
+    public void removeDuplicate() {
+        if(mHead.next == null) {
+            return;
+        }
+        HashSet<T> set = new HashSet<>();
+        Node<T> p = mHead;
+        while (p.next != null) {
+            if(!set.add(p.next.data)) {
+                removeNode(p);
+                continue;
+            }
+            p = p.next;
+        }
+    }
+
+    public void tailPointTo(int position) {
+        if(position < 0 || position >= length) {
+            return;
+        }
+        Node<T> p = mHead.next;
+        while (position-- > 0) {
+            p = p.next;
+        }
+        Node<T> tmp = p;
+        while (p.next != null) {
+            p = p.next;
+        }
+        p.next = tmp;
+    }
+
+    public boolean isLoop() {
+        if(mHead.next == null) {
+            return false;
+        }
+        Node<T> fast = mHead.next;
+        Node<T> slow = mHead.next;
+        while (fast != null && fast.next != null) {
+            if(fast.next == slow || fast.next.next == slow) {
+                return true;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return false;
+    }
+
+    public void tailPointTo(SimpleList<T> list, int position) {
+        if(mHead.next == null || list.getLength() <= position || position < 0) {
+            return;
+        }
+        getTailNode().next = list.getNode(position);
+        length += list.getLength() - position;
+    }
+
+    public boolean isIntersect(SimpleList<T> list) {
+        if(mHead.next == null || list.getLength() == 0) {
+            return false;
+        }
+        return getTailNode() == list.getTailNode();
+    }
+
+    public int intersectAt(SimpleList<T> list) {
+        if(mHead.next == null || list.getLength() == 0) {
+            return -1;
+        }
+        int position = 0;
+        Node<T> p1, p2;
+        if(length > list.getLength()) {
+            position = length - list.getLength();
+            p1 = getNode(position);
+            p2 = list.getHeadNode();
+        } else {
+            p1 = getHeadNode();
+            p2 = list.getNode(list.getLength() - length);
+        }
+        while (p1 != null && p2 != null) {
+            p1 = p1.next;
+            p2 = p2.next;
+            position++;
+            if(p1 == p2) {
+                return position;
+            }
+        }
+        return -1;
+    }
+
+    private Node<T> getNode(int position) {
+        if(position < 0 || position >= length) {
+            return null;
+        }
+        Node<T> node = mHead.next;
+        while (position-- > 0) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public Node<T> getHeadNode() {
+        return mHead.next;
+    }
+
+    public Node<T> getTailNode() {
+        if(mHead.next == null) {
+            return null;
+        }
+        Node<T> tail = mHead.next;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        return tail;
+    }
+
+    public String toStringFromTail() {
+        StringBuilder builder = new StringBuilder("[");
+        Node<T> p = mHead.next;
+        Stack<T> stack = new Stack<>();
+        while (p != null) {
+            stack.push(p.data);
+            p = p.next;
+        }
+        while (stack.size() > 0) {
+            builder.append(stack.pop().toString());
+            builder.append(", ");
+        }
+        builder.delete(builder.length() - 2, builder.length());
+        builder.append("]");
+        return builder.toString();
+    }
+
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder("[");
@@ -102,5 +216,12 @@ public class SimpleList<T> {
         builder.delete(builder.length() - 2, builder.length());
         builder.append("]");
         return builder.toString();
+    }
+
+    private void removeNode(Node<T> prev) {
+        Node<T> tmp = prev.next;
+        prev.next = prev.next.next;
+        tmp.next = null;
+        length--;
     }
 }
