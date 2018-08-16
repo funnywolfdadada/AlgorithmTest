ba7a78b6a3add1eb50b4d4414eb029b8a7705597
zhaodongliang <zhaodongliang1@xiaomi.com>
2018-08-16 11:19:19 +0800

    singleton

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import heap.HeapTest;
 import list.ListTest;
 import queue.QueueTest;
+import singleton.SingletonTest;
 import sorting.SortingTest;
 import stack.StackTest;
 
 public class Main {
 
     public static void main(String[] args) {
-        HeapTest.heapTest(6);
+        SingletonTest.test();
     }
 
 }
---
+++ src/singleton/SingletonTest.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class SingletonTest {
+    public static void test() {
+        System.out.println("SingletonTest start");
+        System.out.println("load InnerClassSingleton start");
+        try {
+            Class.forName("singleton.InnerClassSingleton");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println("load InnerClassSingleton end");
+        InnerClassSingleton.getInstance().test();
+        System.out.println("SingletonTest end");
+    }
+}
